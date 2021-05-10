95c157dd29974cf4e7094a029566f3d9026cdf0f
dani <admoscop@gmail.com>
2021-05-11 04:23:22 +0700

    making a level

--- Player/Scripts/Player.gd
+++ Player/Scripts/Player.gd
@@ -21,6 +21,7 @@ onready var camera_view: Camera = get_node("CameraRoot/CameraBase/SpringArm/Came
 func _ready() -> void:
 	orientation = model.global_transform
 	orientation.origin = Vector3.ZERO
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta: float) -> void:
 	var motion_target = Vector2(
@@ -38,14 +39,17 @@ func _physics_process(delta: float) -> void:
 	camera_z.y = 0
 	camera_z = camera_z.normalized()
 	
-	if motion.length() > 0.001:
+	if motion_target.length() > 0:
 		var target = camera_x * motion.x - camera_z * motion.y
 		if target.length() > 0.001:
 			var q_from = orientation.basis.get_rotation_quat()
 			var q_to = Transform().looking_at(target, Vector3.UP).basis.get_rotation_quat()
 			orientation.basis = Basis(q_from.slerp(q_to, 10 * delta))
-			animation_tree['parameters/walk/blend_position'] = motion.length()
-			root_motion = animation_tree.get_root_motion_transform()
+		animation_tree['parameters/walk/blend_position'] = motion.length()
+		root_motion = animation_tree.get_root_motion_transform()
+	else:
+		animation_tree['parameters/walk/blend_position'] = motion.length()
+		return
 	
 	orientation *= root_motion
 	var hvel: Vector3 = orientation.origin / delta
@@ -67,4 +71,9 @@ func _input(event: InputEvent) -> void:
 		camera_x_root += event.relative.y * 0.002
 		camera_x_root = clamp(camera_x_root, deg2rad(-40), deg2rad(40))
 		camera_base.rotation.x = camera_x_root
+	if event.is_action_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
